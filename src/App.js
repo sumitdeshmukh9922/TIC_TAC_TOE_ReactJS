1ab6551064eead2b321fe1672cd9352cb3878842
sumitdeshmukh9922 <sumitdeshmukh9922@gmail.com>
2021-02-27 20:44:50 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
+import React, {useState} from "react";
 import logo from './logo.svg';
 import './App.css';
-import Icon from "./components/icon"
+import Icon from "./components/icon";
 
-function App() {
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+import {Card, CardBody, Container, Button, Col, Row} from "reactstrap";
+import 'bootstrap/dist/css/bootstrap.css';
+
+const itemArray = new Array(9).fill("empty")
+
+const App = () => {
+
+  const [isCross, setIsCross] = useState(false)
+  const [winMessage, setWinMessage] = useState("")
+
+  const reloadGame = () => {
+
+  }
+
+  const checkIsWinner = () => {
+    
+  }
+
+  const changeItem = itemNumber => {
+    
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Icon />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="p-5">
+      <ToastContainer position="bottom-center" />
+      <Row>
+        <Col md={6} className="offset-md-3">
+          <div className="grid">
+            {itemArray.map((item, index) => (
+              <Card>
+                <CardBody  className="box">
+                  <Icon name={item} />
+                </CardBody>
+              </Card>
+            ) )}
+          </div>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
--- src/components/icon.js
+++ src/components/icon.js
@@ -2,13 +2,15 @@ import React from "react"
 import {FaTimes, FaPen, FaRegCircle} from "react-icons/fa"
 
 const Icon = ({name}) => {
-    return(
-        <h1>
-            <FaTimes className="icon" />
-            <FaPen className="icon" />
-            <FaRegCircle className="icon" />
-        </h1>
-    )
+   switch (name) {
+       case 'circle':
+           return <FaRegCircle className="icons" />
+       case 'cross':
+           return <FaTimes className="icons" />
+       default:
+           return <FaPen className="icons" />   
+           
+   }
 }
 
 export default Icon;
